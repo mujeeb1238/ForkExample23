fb751c9e8bfdc5dd75544d3eb384a18629ed349f
AshokReddy556 <ashokreddy556@gmail.com>
2013-04-23 15:46:59 +0530

    Hello

--- src/packagefork/Fork.java
+++ src/packagefork/Fork.java
@@ -9,8 +9,15 @@ public class Fork {
 		int z=x+y;
 		System.out.println(z);
 	}
+	public static void sub(){
+	x=15;
+	y=14;
+	int z=x-y;
+	System.out.println(z);
+	}
 	public static void main(String a[]){
 		sum();
+                sub();
 		System.out.println("Hello hai how r u");
 	}
 
